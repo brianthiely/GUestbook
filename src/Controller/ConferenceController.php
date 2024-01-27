4c3d464fc152b142140a8c1d433558d102c1c422
Brian <brian.thiely@gmail.com>
2024-01-27 14:19:52 +0100

    Add & config easyadmin

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/', name: 'app_conference')]
+    #[Route('/', name: 'homepage')]
     public function index(): Response
     {
         return new Response(<<<EOF
